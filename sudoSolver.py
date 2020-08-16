4f5bbeac1d5e815b305faa7c3f8f7b9a088f6ea4
Jérôme Henry-Barnaudière - JHB <henry-barnaudiere.j@allier.fr>
2020-08-16 14:02:48 +0200

    v0.1.10

--- sudoSolver.py
+++ sudoSolver.py
@@ -13,11 +13,47 @@
 #   Date        :   8 aout 2020
 #
 
+from cmdLineParser import cmdLineParser
+from colorizer import colorizer, backColor, textColor, textAttribute    # Pour la coloration des sorties terminal
+
 from sudoku import sudoku
 from ownExceptions import sudokuError
 
-# Gestion de la grille
+# Constantes de l'application
 #
+
+# Version du programme
+CURRENT_VERSION = "0.1.10"
+
+# Options de la ligne de commandes
+#
+
+CMD_OPTION_CHAR = "-"            # Une option débute par ce caractère
+
+CMD_OPTION_EDIT = "e"            # Edition d'une (nouvelle) grille
+CMD_OPTION_CONSOLE = "c"         # Affichages en mode console (term ou nCurses ni dispo)
+CMD_OPTION_DETAILS = "d"         # Niveau de détail (0 = aucun)
+
+#
+#   Fonctions
+#
+
+# Usage de l'application
+#
+def _usage(color):
+    print(color.colored("\nsudoSolver.py", formatAttr=[textAttribute.GRAS]), "- version", CURRENT_VERSION)
+    print("\t", color.colored(CMD_OPTION_CHAR + CMD_OPTION_EDIT + " {destName} ", formatAttr=[textAttribute.FONCE]), ": Lancement en mode édition. Le fichier {destName} est crée ou modifié")
+    print("\t", color.colored(CMD_OPTION_CHAR + CMD_OPTION_CONSOLE, formatAttr=[textAttribute.FONCE]), ": Affichage en mode console (term ou nCurses si disponible)")
+    print("\t", color.colored(CMD_OPTION_CHAR + CMD_OPTION_DETAILS + " {drawFreq} ",formatAttr=[textAttribute.FONCE]),": Fréqeunce d'affiche des grilles en cours de résolution (0 = aucun, 1 : 100%, 10 = 1/10, 100 = 1/100,  ...")
+
+#
+# Point d'entrée de l'application
+#
+
+# C'est parti ...
+color = colorizer(True)
+_usage(color)
+
 try:
     solver = sudoku()
     solver.loadFromFile("/Users/jhenry-barnaudiere/Nextcloud/dev/python/sudoSolver/grid2.txt")
